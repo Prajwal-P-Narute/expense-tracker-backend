7e6fe47b20b4965f55ea58d7fe108db40fb2144f
prajwalnarute <prajwalnarute100@gmail.com>
2025-08-05 07:55:05 +0530

    changes

--- src/main/java/com/my/expense_tracker_ui/repository/TransactionRepository.java
+++ src/main/java/com/my/expense_tracker_ui/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.my.expense_tracker_ui.repository;
 
 import com.my.expense_tracker_ui.model.Transaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
 }
